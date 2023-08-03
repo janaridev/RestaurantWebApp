9d4889bc21897c98eee992f1c9547aaf2e468fb5
Raul Janari <rdjanari1707@gmail.com>
2023-08-04 00:38:49 +0200

    Sign up and login funcs

--- backend/authService/main.go
+++ backend/authService/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"authService/controllers"
 	"authService/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -14,11 +15,9 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+
 	r.Run()
 }
