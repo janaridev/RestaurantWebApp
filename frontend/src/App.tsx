5976ac78bac6d218853d79206fa2a4132207e69d
Raul Janari <rdjanari1707@gmail.com>
2023-08-09 23:32:33 +0200

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import Navbar from "./pages/navbar";
+
 const App = () => {
-  return "Hello World";
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
