acabf5255401d6456469d1180c78bc967fa8fafb
Raul Janari <rdjanari1707@gmail.com>
2023-08-20 01:10:40 +0200

    allow localhost

--- src/backend/couponService/src/server.ts
+++ src/backend/couponService/src/server.ts
@@ -5,7 +5,7 @@ import cors from "@fastify/cors";
 function buildServer() {
   const server: FastifyInstance = Fastify();
   server.register(cors, {
-    origin: `http://127.0.0.1`,
+    origin: ["http://127.0.0.1", "http://localhost"],
   });
   server.register(registryRoutes, { prefix: "/api" });
 
