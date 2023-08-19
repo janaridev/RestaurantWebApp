7e4d9af55719799b4f7bdf53fe82da3e6d11d7e8
Raul Janari <rdjanari1707@gmail.com>
2023-08-20 00:35:09 +0200

    test ci

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  console.log("Test CI");
   return (
     <>
       <Navbar />
