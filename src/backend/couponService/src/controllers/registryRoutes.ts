193b12b6db2fd46151e3851beae2140998a680c7
Raul Janari <rdjanari1707@gmail.com>
2023-08-27 01:56:14 +0200

    protecting routes in coupon service

--- src/backend/couponService/src/controllers/registryRoutes.ts
+++ src/backend/couponService/src/controllers/registryRoutes.ts
@@ -7,18 +7,43 @@ export async function registryRoutes(server: FastifyInstance): Promise<void> {
   const couponController = new CouponController(couponService);
 
   // In your route registration:
-  server.get("/coupons", couponController.getCoupons.bind(couponController));
+  server.get(
+    "/coupons",
+    {
+      preHandler: [server.auth],
+    },
+    couponController.getCoupons.bind(couponController)
+  );
+
   server.get(
     "/coupons/:couponId",
+    {
+      preHandler: [server.auth],
+    },
     couponController.getCouponById.bind(couponController)
   );
-  server.post("/coupons", couponController.createCoupon.bind(couponController));
+
+  server.post(
+    "/coupons",
+    {
+      preHandler: [server.auth],
+    },
+    couponController.createCoupon.bind(couponController)
+  );
+
   server.put(
     "/coupons/:couponId",
+    {
+      preHandler: [server.auth],
+    },
     couponController.updateCoupon.bind(couponController)
   );
+
   server.delete(
     "/coupons/:couponId",
+    {
+      preHandler: [server.auth],
+    },
     couponController.deleteCoupon.bind(couponController)
   );
 }
