4f91f5d7e5e12fe3b4030e7ee20373327a8e2609
Raul Janari <rdjanari1707@gmail.com>
2023-08-28 01:12:02 +0200

    defining zod schemas

--- src/backend/productService/src/product/product.shemas.ts
+++ src/backend/productService/src/product/product.shemas.ts
@@ -1,5 +1,8 @@
 import { Schema } from "mongoose";
+import { z } from "zod";
+import { buildJsonSchemas } from "fastify-zod";
 
+// MONGOOSE SCHEMA
 export const ProductSchema = new Schema({
   name: {
     type: String,
@@ -21,4 +24,34 @@ export const ProductSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+// ZOD SCHEMAS
+const productSchema = z.object({
+  name: z.string({
+    required_error: "Product name is required.",
+    invalid_type_error: "Product name must be a string.",
+  }),
+  price: z.number({
+    required_error: "Product price is required.",
+    invalid_type_error: "Product price must be a number.",
+  }),
+  description: z.string({
+    required_error: "Product description is required.",
+    invalid_type_error: "Product description must be a string.",
+  }),
+  categoryName: z.string({
+    required_error: "Product category name is required.",
+    invalid_type_error: "Product category name must be a string",
+  }),
+  imageUrl: z.string({
+    required_error: "Product image url is required.",
+    invalid_type_error: "Product image url must be a string",
+  }),
+});
+
+export type ProductInput = z.infer<typeof productSchema>;
+
+export const { schemas: productSchemas, $ref } = buildJsonSchemas({
+  productSchema,
 });
--- src/backend/productService/src/server.ts
+++ src/backend/productService/src/server.ts
@@ -1,11 +1,16 @@
 import Fastify, { FastifyInstance } from "fastify";
 import cors from "@fastify/cors";
+import { productSchemas } from "product/product.shemas";
 
 function buildServer() {
   const server: FastifyInstance = Fastify();
   server.register(cors, {
     origin: ["http://127.0.0.1", "http://localhost", "http://localhost:3000"],
   });
+
+  for (const schema of [...productSchemas]) {
+    server.addSchema(schema);
+  }
 
   return server;
 }
