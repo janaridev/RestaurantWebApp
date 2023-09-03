2faf8d6722fc987677ee910edd368ec6776062f6
Raul Janari <rdjanari1707@gmail.com>
2023-09-03 21:58:30 +0200

    Turning all properties to init in dto

--- src/backend/shoppingCartService/Dtos/CartHeaderDto.cs
+++ src/backend/shoppingCartService/Dtos/CartHeaderDto.cs
@@ -2,9 +2,9 @@ namespace Dtos;
 
 public record CartHeaderDto
 {
-    public Guid CartHeaderId { get; set; }
-    public string UserId { get; set; }
-    public string CouponCode { get; set; }
-    public double Discount { get; set; }
-    public double CartTotal { get; set; }
+    public Guid CartHeaderId { get; init; }
+    public string UserId { get; init; }
+    public string CouponCode { get; init; }
+    public double Discount { get; init; }
+    public double CartTotal { get; init; }
 }
