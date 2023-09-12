97aeadeaeb369401b573c2fa507cbd9fcb0947cc
Raul Janari <rdjanari1707@gmail.com>
2023-09-12 08:13:03 +0200

    based on state show cart or image

--- src/frontend/src/pages/cart/index.tsx
+++ src/frontend/src/pages/cart/index.tsx
@@ -38,6 +38,7 @@ const Cart = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [cart, setCart] = useState<Cart>();
+  const [isCartEmpty, setIsCartEmpty] = useState<Boolean>(true);
 
   const getCart = async () => {
     try {
@@ -50,6 +51,11 @@ const Cart = () => {
         }
       );
       setCart(response.data.result);
+      if (response.data.result === null) {
+        setIsCartEmpty(true);
+      } else {
+        setIsCartEmpty(false);
+      }
     } catch (error) {
       if (axios.isAxiosError(error)) {
         const axiosError = error as AxiosError;
@@ -174,115 +180,132 @@ const Cart = () => {
   };
 
   return (
-    <form method="post">
-      <br />
-      <div className="">
-        <div className="card border mt-4 container">
-          <div className="card-header bg-dark text-light ml-0 row ">
-            <div className="col-6">
-              <h3 className="text-success">
-                <i className="bi bi-cart"></i> &nbsp; Shopping Cart
-              </h3>
-            </div>
-            <div className="col-6 text-end">
-              <a className="btn btn-outline-warning mt-2 btn-sm" href="/">
-                Continue Shopping
-              </a>
-            </div>
-          </div>
-          <div className="card-body">
-            <div className=" d-none d-lg-block">
-              <div className="row text-info">
-                <div className="col-lg-2"></div>
-                <div className="col-lg-5">Product Details</div>
-                <div className="col-lg-2">Price</div>
-                <div className="col-lg-2">Count</div>
-                <div className="col-lg-1"></div>
-              </div>
-            </div>
-            <hr />
-
-            {cart?.cartDetails.map((cartDetail: CartDetails) => {
-              return (
-                <div className="row h-100" key={cartDetail.cartDetailsId}>
-                  <div className="col-4 col-md-2 text-center py-2">
-                    <img
-                      src={cartDetail.product.imageUrl}
-                      className="rounded"
-                      width="100%"
-                    />
-                  </div>
-                  <div className="col-8 col-md-5">
-                    <h5>{cartDetail.product.name}</h5>
-                    <div style={{ fontSize: "11px" }}>
-                      {cartDetail.product.description}
-                    </div>
-                  </div>
-                  <div
-                    className="col-3 col-md-2 pt-md-4"
-                    style={{ fontSize: "11px" }}
-                  >
-                    <span style={{ fontSize: "17px" }}>
-                      {formatCurrency(cartDetail.product.price)}
-                    </span>
-                  </div>
-                  <div
-                    className="col-3 col-md-2 pt-md-4"
-                    style={{ fontSize: "11px" }}
-                  >
-                    <span style={{ fontSize: "17px" }}>{cartDetail.count}</span>
-                  </div>
-                  <div
-                    className="col-2 col-lg-1 p-0 pt-lg-4 text-center"
-                    onClick={() => deleteCartDetail(cartDetail.cartDetailsId)}
-                  >
-                    <a className="btn btn-sm btn-danger">
-                      <i className="bi bi-trash-fill"></i>
-                    </a>
-                  </div>
-                </div>
-              );
-            })}
-
-            <hr />
-            <div className="row">
-              <div className="col-6">
-                <span>Coupon:</span>
-                <input />
-                <button type="submit" className="btn btn-sm btn-success">
-                  Apply
-                </button>
-              </div>
-              <div className="col-6 text-end">
-                <span className="text-danger" style={{ fontSize: "21px" }}>
-                  {" "}
-                  Order Total : {formatCurrency(
-                    cart?.cartHeader.cartTotal
-                  )}{" "}
-                  <br />
-                </span>
-                <span className="text-success"> Order Discount : $$</span>
-              </div>
-            </div>
-          </div>
-          <div className="card-footer row">
-            <div className="col-6 col-md-3 ">
-              <button
-                type="submit"
-                className="btn btn-outline-danger form-control"
-              >
-                Email Cart
-              </button>
-            </div>
-            <div className="col-6 col-md-3 offset-md-6">
-              <button disabled className="btn btn-success form-control ">
-                Checkout (Comming Soon!)
-              </button>
-            </div>
+    <>
+      {isCartEmpty ? (
+        <div className="container">
+          <div className="row">
+            <img
+              src="https://assets.materialup.com/uploads/16e7d0ed-140b-4f86-9b7e-d9d1c04edb2b/preview.png"
+              alt="cart is empty"
+            />
           </div>
         </div>
-      </div>
-    </form>
+      ) : (
+        <form method="post">
+          <br />
+          <div className="">
+            <div className="card border mt-4 container">
+              <div className="card-header bg-dark text-light ml-0 row ">
+                <div className="col-6">
+                  <h3 className="text-success">
+                    <i className="bi bi-cart"></i> &nbsp; Shopping Cart
+                  </h3>
+                </div>
+                <div className="col-6 text-end">
+                  <a className="btn btn-outline-warning mt-2 btn-sm" href="/">
+                    Continue Shopping
+                  </a>
+                </div>
+              </div>
+              <div className="card-body">
+                <div className=" d-none d-lg-block">
+                  <div className="row text-info">
+                    <div className="col-lg-2"></div>
+                    <div className="col-lg-5">Product Details</div>
+                    <div className="col-lg-2">Price</div>
+                    <div className="col-lg-2">Count</div>
+                    <div className="col-lg-1"></div>
+                  </div>
+                </div>
+                <hr />
+
+                {cart?.cartDetails.map((cartDetail: CartDetails) => {
+                  return (
+                    <div className="row h-100" key={cartDetail.cartDetailsId}>
+                      <div className="col-4 col-md-2 text-center py-2">
+                        <img
+                          src={cartDetail.product.imageUrl}
+                          className="rounded"
+                          width="100%"
+                        />
+                      </div>
+                      <div className="col-8 col-md-5">
+                        <h5>{cartDetail.product.name}</h5>
+                        <div style={{ fontSize: "11px" }}>
+                          {cartDetail.product.description}
+                        </div>
+                      </div>
+                      <div
+                        className="col-3 col-md-2 pt-md-4"
+                        style={{ fontSize: "11px" }}
+                      >
+                        <span style={{ fontSize: "17px" }}>
+                          {formatCurrency(cartDetail.product.price)}
+                        </span>
+                      </div>
+                      <div
+                        className="col-3 col-md-2 pt-md-4"
+                        style={{ fontSize: "11px" }}
+                      >
+                        <span style={{ fontSize: "17px" }}>
+                          {cartDetail.count}
+                        </span>
+                      </div>
+                      <div
+                        className="col-2 col-lg-1 p-0 pt-lg-4 text-center"
+                        onClick={() =>
+                          deleteCartDetail(cartDetail.cartDetailsId)
+                        }
+                      >
+                        <a className="btn btn-sm btn-danger">
+                          <i className="bi bi-trash-fill"></i>
+                        </a>
+                      </div>
+                    </div>
+                  );
+                })}
+
+                <hr />
+                <div className="row">
+                  <div className="col-6">
+                    <span>Coupon:</span>
+                    <input />
+                    <button type="submit" className="btn btn-sm btn-success">
+                      Apply
+                    </button>
+                  </div>
+                  <div className="col-6 text-end">
+                    <span className="text-danger" style={{ fontSize: "21px" }}>
+                      {" "}
+                      Order Total : {formatCurrency(
+                        cart?.cartHeader.cartTotal
+                      )}{" "}
+                      <br />
+                    </span>
+                    <span className="text-success"> Order Discount : $$</span>
+                  </div>
+                </div>
+              </div>
+              <div className="card-footer row">
+                <div className="col-6 col-md-3 ">
+                  <button
+                    type="submit"
+                    className="btn btn-outline-danger form-control"
+                  >
+                    Email Cart
+                  </button>
+                </div>
+                <div className="col-6 col-md-3 offset-md-6">
+                  <button disabled className="btn btn-success form-control ">
+                    Checkout (Comming Soon!)
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
