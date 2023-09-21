32b974f400747d02bb0d35f43fa6829525bf472c
Raul Janari <rdjanari1707@gmail.com>
2023-09-21 13:49:29 +0200

    imported env, refactored hardCoded port

--- src/backend/orderService/src/main.ts
+++ src/backend/orderService/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
+  ConfigModule.forRoot({
+    envFilePath: '.env',
+  });
   const app = await NestFactory.create(AppModule);
-  await app.listen(3050);
+  await app.listen(Number(process.env.PORT));
 }
 bootstrap();
