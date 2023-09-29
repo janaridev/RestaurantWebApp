7ec47499fa1a99999f7f6bb8284b96257cf68063
Raul Janari <rdjanari1707@gmail.com>
2023-09-29 06:04:20 +0200

    gin framework setup & health check endpoint

--- src/backend/orderService/main.go
+++ src/backend/orderService/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello Go")
+	router := gin.Default()
+	router.GET("/api/healthCheck", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "Health Check succeed."})
+	})
+	router.Run("localhost:3050")
 }
