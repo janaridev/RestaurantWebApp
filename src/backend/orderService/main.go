05d8e0dbbd6f9f2715702fcdb4c49337ac2cccf7
Raul Janari <rdjanari1707@gmail.com>
2023-09-29 06:16:30 +0200

    env file load & no hardcoded server url

--- src/backend/orderService/main.go
+++ src/backend/orderService/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"orderService/initializers"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/api/healthCheck", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"message": "Health Check succeed."})
 	})
-	router.Run("localhost:3050")
+	router.Run(os.Getenv("SERVER_URL"))
 }
