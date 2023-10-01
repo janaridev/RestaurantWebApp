b192516afe9c6080a9368bd410fc9f68a419eb8c
Raul Janari <rdjanari1707@gmail.com>
2023-10-01 08:18:07 +0200

    order statuses constants

---
+++ src/backend/orderService/constants/orderStatus.go
@@ -0,0 +1,8 @@
+package constants
+
+const Status_Pending = "Pending"
+const Status_Approved = "Approved"
+const Status_ReadyForPickUp = "ReadyForPickup"
+const Status_Completed = "Completed"
+const Status_Refunded = "Refunded"
+const Status_Canselled = "Cancelled"
